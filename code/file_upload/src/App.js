7c96f8e018bc68d43c5eb4863e30640d63e02bed
AzureQ <65605194+Azure-Q@users.noreply.github.com>
2020-07-25 19:45:48 +0800

    Update App.js

--- code/file_upload/src/App.js
+++ code/file_upload/src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import {HashRouter as Router, Route} from 'react-router-dom';
 import MyTemplate from "./jsPages/template/template";
-//import Test from "./tests/test";
+import Test from "./tests/test";
 
 function App() {
     return (
         <Router>
             <div>
                 <Route exact path={"/"} component={MyTemplate}/>
-                {/*<Route path={'/test'} component={Test}/>*/}
+                <Route path={'/test'} component={Test}/>
             </div>
         </Router>
     );
