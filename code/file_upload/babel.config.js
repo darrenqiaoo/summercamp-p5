04fbc869fe5ec4805e18845f6b31e19cc138ea62
AzureQ <65605194+Azure-Q@users.noreply.github.com>
2020-07-28 14:37:03 +0800

    Update babel.config.js

--- code/file_upload/babel.config.js
+++ code/file_upload/babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     presets: ['@babel/preset-env', '@babel/preset-react'],
-    plugins: ['@babel/plugin-proposal-class-properties']
+    plugins: ['@babel/plugin-proposal-class-properties',"transform-es2015-modules-commonjs"]
   };
