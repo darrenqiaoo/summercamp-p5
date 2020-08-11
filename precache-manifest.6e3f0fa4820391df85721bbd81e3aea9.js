df9bc39194f643ab817270578e287833e05f3f58
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-11 00:25:45 +0000

    Deploy darrenqiaoo/summercamp-p5 to github.com/darrenqiaoo/summercamp-p5.git:gh-pages

---
+++ precache-manifest.6e3f0fa4820391df85721bbd81e3aea9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e7670d7b38649445cd8c909f920f4be",
+    "url": "/summercamp-p5/index.html"
+  },
+  {
+    "revision": "41cc2f3da61f7ba91796",
+    "url": "/summercamp-p5/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "2a079026a13c5bde466a",
+    "url": "/summercamp-p5/static/css/main.24a021b8.chunk.css"
+  },
+  {
+    "revision": "41cc2f3da61f7ba91796",
+    "url": "/summercamp-p5/static/js/2.235f1907.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/summercamp-p5/static/js/2.235f1907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a079026a13c5bde466a",
+    "url": "/summercamp-p5/static/js/main.10bac716.chunk.js"
+  },
+  {
+    "revision": "e63d8b19ed85c63bbc11",
+    "url": "/summercamp-p5/static/js/runtime-main.fb647904.js"
+  }
+]);
